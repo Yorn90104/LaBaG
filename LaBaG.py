9ac34643cde470231ab57967d7b9e7efa5fcc93d
FANYU <bingxiao526@gmail.com>
2024-09-03 22:10:14 +0800

    函數簡化

--- LaBaG.py
+++ LaBaG.py
@@ -1,3 +1,4 @@
+import random
 
 ram1 = 0
 ram2 = 0
@@ -10,6 +11,37 @@ add = 0
 times = 30
 ed = 0
 
+def change(x,y) :
+
+      if y <= 36 :
+            x = 'a'
+      elif 36 < y <= 60 :
+            x = 'b'
+      elif 60 < y <= 77 :
+            x = 'c'
+      elif 77 < y <= 89 :
+            x = 'd'
+      elif 89 < y <= 97 :
+            x = 'e'
+      else :
+            x = 'f'
+      return x
+
+def only(x,y) :
+      if x == 'a':
+            y = y + 100
+      elif x == 'b' :
+            y = y + 170
+      elif x == 'c' :
+            y = y + 780
+      elif x == 'd' :
+            y = y + 870
+      elif x == 'e' :
+            y = y + 5000
+      elif x == 'f' :
+            y = y + 10000
+      return y
+
 print("共 {} 次".format(times))
 
 while ed < times :
@@ -18,50 +50,19 @@ while ed < times :
     button=input("按下 ENTER 開始")
     if button == "" :
         #隨機數
-        import random
         ram1 = random.randint(1,100)
         ram2 = random.randint(1,100)
         ram3 = random.randint(1,100)
 
         #1
-        if ram1 <= 36 :
-            p1 = 'a'
-        elif 36 < ram1 <= 60 :
-            p1 = 'b'
-        elif 60 < ram1 <= 77 :
-            p1 = 'c'
-        elif 77 < ram1 <= 89 :
-            p1 = 'd'
-        elif 89 < ram1 <= 97 :
-            p1 = 'e'
-        else :
-            p1 = 'f'
+        p1 = change(p1,ram1)
+
         #2
-        if ram2 <= 36 :
-            p2 = 'a'
-        elif 36 < ram2 <= 60 :
-            p2 = 'b'
-        elif 60 < ram2 <= 77 :
-            p2 = 'c'
-        elif 77 < ram2 <= 89 :
-            p2 = 'd'
-        elif 89 < ram2 <= 97 :
-            p2 = 'e'
-        else :
-            p2 = 'f'
+        p2 = change(p2,ram2)
+
         #3
-        if ram3 <= 36 :
-            p3 = 'a'
-        elif 36 < ram3 <= 60 :
-            p3 = 'b'
-        elif 60 < ram3 <= 77 :
-            p3 = 'c'
-        elif 77 < ram3 <= 89 :
-            p3 = 'd'
-        elif 89 < ram3 <= 97 :
-            p3 = 'e'
-        else :
-            p3 = 'f'
+        p3 = change(p3,ram3)
+
 
         print(p1,' ',p2,' ',p3)
 
@@ -175,44 +176,14 @@ while ed < times :
         #3個都不同 加總/3
         elif p1 != p2 != p3 :
             #1
-            if p1 == 'a':
-                add = add + 30
-            elif p1 == 'b' :
-                add = add + 50
-            elif p1 == 'c' :
-                add = add + 250
-            elif p1 == 'd' :
-                add = add + 290
-            elif p1 == 'e' :
-                add = add + 1200
-            elif p1 == 'f' :
-                add = add + 2500
+            add = only(p1,add)
+            
             #2
-            if p2 == 'a':
-                add = add + 30
-            elif p2 == 'b' :
-                add = add + 50
-            elif p2 == 'c' :
-                add = add + 250
-            elif p2 == 'd' :
-                add = add + 290
-            elif p2 == 'e' :
-                add = add + 1200
-            elif p2 == 'f' :
-                add = add + 2500
+            add = only(p2,add)
+            
             #3
-            if p3 == 'a':
-                add = add + 30
-            elif p3 == 'b' :
-                add = add + 50
-            elif p3 == 'c' :
-                add = add + 250
-            elif p3 == 'd' :
-                add = add + 290
-            elif p3 == 'e' :
-                add = add + 1200
-            elif p3 == 'f' :
-                add = add + 2500
+            add = only(p3,add)
+           
             add = round( add / 3 )
 
         score = score + add
