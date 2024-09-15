f915beb87807080b8817cc3608f986b4bdc8b733
FANYU <bingxiao526@gmail.com>
2024-09-15 12:36:33 +0800

    背景隨視窗大小 & 分數清單

--- LABA/LaBaG.py
+++ LABA/LaBaG.py
@@ -3,10 +3,42 @@ import tkinter as tk
 from PIL import Image , ImageTk
 from random import randint #隨機數字
 
+
+def IMAGE(file , w , h) :
+    "處理成TK可識別的圖"
+    "(路徑,長,寬)"
+    pc = Image.open(file)  
+    pc = pc.resize((w, h), Image.LANCZOS)  # 调整图片大小
+    pc = ImageTk.PhotoImage(pc)
+    return pc
+
+def resize_BG(event):
+    #獲取當前長寬
+    New_width = event.width
+    New_height = event.height
+
+    #調整大小&更新圖片
+    New_BG = IMAGE(r'.\Asset\BG.png' , New_width , New_height)
+    canvas.delete("bg") #刪除標記為"bg"的圖片
+    canvas.create_image(0, 0, image=New_BG, anchor="nw", tags="bg") #新圖片
+
+    #保存圖像引用 防止被回收(New_BG為非全域變數)
+    canvas.image = New_BG
+
 win = tk.Tk() #建立視窗
 win.title("啦八機") #視窗標題
 win.iconbitmap(r'.\Asset\Superhhh.ico') #視窗小圖
 win.geometry("640x360") #視窗長寬
+
+BG = IMAGE(r'.\Asset\BG.png' , 640 , 360)
+
+# 创建 Canvas & 设置背景图片
+canvas = tk.Canvas(win, width=640, height=360)
+canvas.pack(fill="both", expand=True) #在視窗中水平和垂直方向上擴展，以填充其父容器的空間
+canvas.create_image(0, 0, image=BG, anchor="nw" , tags = "bg") #初次加載圖片 & "nw" = north west & 標記圖片為"bg"
+
+#綁定視窗大小變化事件
+win.bind('<Configure>', resize_BG)
 
 win.mainloop() #視窗常駐
 
@@ -22,6 +54,11 @@ score = 0
 add = 0
 times = 30
 ed = 0
+
+#分數清單
+same3 = [20000 , 10000 , 1800 , 1600 , 600 , 200]
+same2 = [10000 ,5000 , 870 , 780 , 170 , 100]
+same1 = [2500 , 1200 , 290 , 250 , 50 , 30]
 
 def change(x,y) :
       '(歸屬,隨機數)'
@@ -39,20 +76,20 @@ def change(x,y) :
             x = 'F'
       return x
 
-def only(x,y) :
-      '(歸屬,增加分)'
+def ADD(x,y,lst) :
+      '(歸屬,增加分,分數清單)'
       if x == 'A':
-            y = y + 100
+            y = y + lst[0]
       elif x == 'B' :
-            y = y + 170
+            y = y + lst[1]
       elif x == 'C' :
-            y = y + 780
+            y = y + lst[2]
       elif x == 'D' :
-            y = y + 870
+            y = y + lst[3]
       elif x == 'E' :
-            y = y + 5000
+            y = y + lst[4]
       elif x == 'F' :
-            y = y + 10000
+            y = y + lst[5]
       return y
 
 def result(s , a , e) :
@@ -90,120 +127,45 @@ while ed < times :
         #增加分數
         #3個相同
         if p1 == p2 == p3 :
-            if p1 == 'A':
-                add = add + 200
-            elif p1 == 'B' :
-                add = add + 600
-            elif p1 == 'C' :
-                add = add + 1600
-            elif p1 == 'D' :
-                add = add + 1800
-            elif p1 == 'E' :
-                add = add + 10000
-            elif p1 == 'F' :
-                add = add + 20000
+            add = ADD(p1 , add , same3)
 
         #2個相同=(2個相同的+1個不同的)/1.3
         # 1 & 2
         elif p1 == p2 :
-            #2個同
-            if p2 == 'A':
-                add = add + 100
-            elif p2 == 'B' :
-                add = add + 170
-            elif p2 == 'C' :
-                add = add + 780
-            elif p2 == 'D' :
-                add = add + 870
-            elif p2 == 'E' :
-                add = add + 5000
-            elif p2 == 'F' :
-                add = add + 10000
+            add = ADD(p1 , add , same2)
             #不同的
-            if p3 == 'A':
-                add = add + 30
-            elif p3 == 'B' :
-                add = add + 50
-            elif p3 == 'C' :
-                add = add + 250
-            elif p3 == 'D' :
-                add = add + 290
-            elif p3 == 'E' :
-                add = add + 1200
-            elif p3 == 'F' :
-                add = add + 2500
+            add = ADD(p3 , add , same1)
+
             add = round( add / 1.3 )
 
         # 2 & 3
         elif p2 == p3 :
             #2個同
-            if p2 == 'A':
-                add = add + 100
-            elif p2 == 'B' :
-                add = add + 170
-            elif p2 == 'C' :
-                add = add + 780
-            elif p2 == 'D' :
-                add = add + 870
-            elif p2 == 'E' :
-                add = add + 5000
-            elif p2 == 'F' :
-                add = add + 10000
+            add = ADD(p2 , add , same2)
             #不同的
-            if p1 == 'A':
-                add = add + 30
-            elif p1 == 'B' :
-                add = add + 50
-            elif p1 == 'C' :
-                add = add + 250
-            elif p1 == 'D' :
-                add = add + 290
-            elif p1 == 'E' :
-                add = add + 1200
-            elif p1 == 'F' :
-                add = add + 2500
+            add = ADD(p1 , add , same1)
+
             add = round( add / 1.3 )
 
         # 1 & 3
         elif p1 == p3 :
             #2個同
-            if p3 == 'A':
-                add = add + 100
-            elif p3 == 'B' :
-                add = add + 170
-            elif p3 == 'C' :
-                add = add + 780
-            elif p3 == 'D' :
-                add = add + 870
-            elif p3 == 'E' :
-                add = add + 5000
-            elif p3 == 'F' :
-                add = add + 10000
+            add = ADD(p3 , add , same2)
             #不同的
-            if p2 == 'A':
-                add = add + 30
-            elif p2 == 'B' :
-                add = add + 50
-            elif p2 == 'C' :
-                add = add + 250
-            elif p2 == 'D' :
-                add = add + 290
-            elif p2 == 'E' :
-                add = add + 1200
-            elif p2 == 'F' :
-                add = add + 2500
+            add = ADD(p2 , add , same1)
+
             add = round( add / 1.3 )
 
         #3個都不同 加總/3
         elif p1 != p2 != p3 :
             #1
-            add = only(p1,add)
+            add = ADD(p1 , add , same1)
             
             #2
-            add = only(p2,add)
+            add = ADD(p2 , add , same1)
             
             #3
-            add = only(p3,add)
+            add = ADD(p3 , add , same1)
            
             add = round( add / 3 )
 
